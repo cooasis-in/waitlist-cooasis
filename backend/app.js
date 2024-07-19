15d1228607887f217a88c6f8be389962112fba07
shreyasCooasis <shreyas@cooasis.in>
2024-07-19 11:51:04 +0530

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -3,21 +3,33 @@ const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const userRoutes = require('./routes/user');
+const cors = require('cors');
 
 const app = express();
 const PORT = 3001;
 
 app.use(bodyParser.json());
+const corsOptions = {
+    origin: 'http://localhost:5173',
+    methods: ['GET', 'POST'],
+    allowedHeaders: ['Content-Type'],
+    credentials: true
+};
 
-// mongoose.connect(process.env.MONGODB_URL, {
-//     useNewUrlParser: true,
-//     useUnifiedTopology: true,
-//     useCreateIndex: true
-// })
-//     .then(() => console.log('MongoDB connected'))
-//     .catch((err) => console.log(err));
+app.use(cors(corsOptions));
 
-app.use('/api', userRoutes);
+const dbUrl = process.env.MONGODB_URL;
+mongoose.connect(dbUrl, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+}).then(() => {
+    console.log("Connection open");
+}).catch(err => {
+    console.log("OH NO ERROR");
+    console.log(err);
+})
+
+app.use('/', userRoutes);
 
 app.listen(PORT, () => {
     console.log(`Server is running on ${PORT}`);
