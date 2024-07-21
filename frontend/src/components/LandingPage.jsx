0b1aa93396e85d4f37d61d2853918268fd66365b
Shahbaz-cooasis <shahbaz@cooasis.in>
2024-07-21 13:44:21 +0530

    transition added

--- frontend/src/components/LandingPage.jsx
+++ frontend/src/components/LandingPage.jsx
@@ -3,56 +3,19 @@ import Typical from "react-typical";
 import axios from "axios";
 import "./pages.css";
 import { Button } from "../ui/moving-border";
-import ReferPage from "./ReferPage";
-import { AnimatedTooltip } from "../ui/animated-tooltip";
+import Refer from "./Refer";
 
 const LandingPage = () => {
   const [showRefer, setShowRefer] = useState(false);
-  const [email, setEmail] = useState("");
 
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-    console.log(email);
-    const content = 
-    "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.";
-    try {
-      const response = await axios.post("http://localhost:3001/users", {
-        email,
-        content,
-      });
-      console.log("User created successfully:", response.data);
-      setShowRefer(true);
-    } catch (error) {
-      console.error(
-        "Error creating user:",
-         error.response ? error.response.data : error.message
-        );
-    }
+  const handleSubmit = (e) => {
+    e.preventDefault(); 
     setShowRefer(true);
-    console.log("Handling submit...");
   };
 
   if (showRefer) {
-    return <ReferPage />;
+    return <Refer />;
   }
-
-  const people = [
-    {
-      id: 1,
-      image:
-        "https://images.unsplash.com/photo-1599566150163-29194dcaad36?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=3387&q=80",
-    },
-    {
-      id: 2,
-      image:
-        "https://images.unsplash.com/photo-1535713875002-d1d0cf377fde?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8YXZhdGFyfGVufDB8fDB8fHww&auto=format&fit=crop&w=800&q=60",
-    },
-    {
-      id: 3,
-      image:
-        "https://images.unsplash.com/photo-1580489944761-15a19d654956?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NXx8YXZhdGFyfGVufDB8fDB8fHww&auto=format&fit=crop&w=800&q=60",
-    },
-  ];
 
   return (
     <section className="bg-color">
@@ -60,27 +23,26 @@ const LandingPage = () => {
         <h1 className="text-[12px] text-center bg-waitlist-gradient bg-clip-text text-transparent f-PowerGrotesk">
           Join waitlist for
         </h1>
-        <div className="reletive">
+        <div className="relative">
           <h2 className="text-[70px] leading-[70px] xxl:text-7xl f-PowerGrotesk text-[#FCFCD8] mb-4 text-center">
-            {/* <span className="bg-nexgen-gradient bg-clip-text">Nex-gen </span> */}
             <Typical
               steps={["Nex-gen", 1000]}
               wrapper="span"
               className="bg-nexgen-gradient bg-clip-text"
             />
-            <Typical
-              steps={["design", 1000]}
+            {/* <Typical
+              steps={["", 1000]}
               wrapper="span"
               className="text-[#FCFCD8]"
-            />
+            /> */}
             <br />
             <Typical
-              steps={["ecosystem", 1000]}
+              steps={["design ecosystem", 1000]}
               wrapper="span"
               className="text-[#FCFCD8]"
             />
           </h2>
-          <div className="absolute top-0 left-[343px]">
+          <div className="absolute top-0 left-[450px] fade-in ">
             <img src="images/james.svg" alt="" />
           </div>
         </div>
@@ -91,20 +53,12 @@ const LandingPage = () => {
               type="email"
               name="email"
               placeholder="Eg. Jeff@cooasis.in"
-              className="f-HelveticaNeueUltraLight bg-transparent	text-[14px] xxl:text-[17px] text-[white] leading-[14.13px] w-[290px] h-[55px] px-6 py-4 mt-0 lg:mt-3 border-[1px] border-[#FFFFFF17] rounded-full custom-inset custom-gradient"
-              value={email}
-              onChange={(e) => setEmail(e.target.value)}
+              className="f-HelveticaNeueUltraLight bg-transparent text-[14px] xxl:text-[17px] text-[white] leading-[14.13px] w-[290px] h-[55px] px-6 py-4 mt-0 lg:mt-3 border-[1px] border-[#FFFFFF17] rounded-full custom-inset custom-gradient"
             />
             <div className="absolute right-[18px] bottom-[20px] cursor-pointer">
               <img src="/images/maillandingpage.svg" alt="Email Icon" />
             </div>
           </div>
-          {/* <button
-            type="submit"
-            className="f-PowerGrotesk bg-[#0000006B] w-[290px] h-[55px] text-[14px] xxl:text-[17px] text-[#E1FF26] leading-[14.13px] px-6 py-4 mt-4 rounded-full flex items-center dark:bg-[#1D2121] justify-center"
-          >
-            Get Early Access
-          </button> */}
           <Button
             borderRadius="2rem"
             className="bg-[#131515] f-PowerGrotesk text-[#E1FF26] rounded-full hover:shadow-lg hover:bg-[#E1FF26] hover:text-black hover:font-bold transform transition-all duration-300 ease-in-out"
@@ -112,7 +66,7 @@ const LandingPage = () => {
             Get Early Access
           </Button>
         </form>
-        <div className="mt-7 ">
+        <div className="mt-7">
           <div className="flex justify-center items-center">
             <img src="/images/avtar-1.svg" alt="People Join" className="overflow-hidden" />
             <img src="/images/avtar-2.svg" alt="People Join" className="ml-[-7px] overflow-hidden" />
@@ -122,15 +76,6 @@ const LandingPage = () => {
             +200 people joined
           </span>
         </div>
-
-        {/* <div className="flex flex-row items-center justify-center mb-2 mt-6 w-full">
-          <AnimatedTooltip items={people} />
-        </div>
-        <span className="f-PowerGrotesk text-[14.5px] text-[#6A92985E] xxl:text-[17.5px] leading-[14.54px] flex justify-center items-center">
-          +200 people joined
-        </span> */}
-
-        {/*  */}
         <div className="relative">
           <div className="flex justify-center items-center mt-11">
             <img src="images/moon-1.svg" alt="" />
@@ -139,7 +84,7 @@ const LandingPage = () => {
             <img
               src="images/moon-2.svg"
               alt=""
-              className=" absolute top-[-35px]"
+              className="absolute top-[-35px]"
             />
           </div>
         </div>
--- frontend/src/components/Refer.jsx
+++ frontend/src/components/Refer.jsx
@@ -14,31 +14,32 @@ const Refer = () => {
   return (
     <div className="bg-colorRefer bg-[#131515] min-h-screen flex flex-col items-center relative overflow-hidden">
       <ConfettiBackground />
-      <div className="gradient-box border-[1px] border-[#FFFFFF21] max-w-[400px] mt-4 rounded-[40px] p-4 text-center shadow-lg relative z-10">
-       <div className="px-[2rem] pt-6">
-       <h1 className="f-PowerGrotesk text-[40px] mx-auto xxl:text-[50px] leading-[40px] text-[#FCFCD8] max-w-[320px]">
-          You are on the <br />
-          <span className="bg-nexgen-gradient bg-clip-text nex-gen-text">
-            waitlist!
-          </span>
-        </h1>
-        <p className="text-[#FFF5D947] mt-8 f-HelveticaNeueLight text-[14px] leading-[19px] xxl:leading-[21px]">
-          We’ve added you to our waitlist. We will notify you once we are ready
-          to launch our beta version. In the meantime, you can share it and get
-          a chance to earn 500 for early access to the platform.
-        </p>
-       </div>
+      <div className="card gradient-box border-[1px] border-[#FFFFFF21] max-w-[400px] mt-4 rounded-[40px] p-4 text-center shadow-lg relative z-10 transform transition-transform duration-300 hover:scale-105 hover:shadow-2xl">
+        <div className="px-[2rem] pt-6">
+          <h1 className="f-PowerGrotesk text-[40px] mx-auto xxl:text-[50px] leading-[40px] text-[#FCFCD8] max-w-[320px]">
+            You are on the <br />
+            <span className="bg-nexgen-gradient bg-clip-text nex-gen-text">
+              waitlist!
+            </span>
+          </h1>
+          <p className="text-[#FFF5D947] mt-8 f-HelveticaNeueLight text-[14px] leading-[19px] xxl:leading-[21px]">
+            We’ve added you to our waitlist. We will notify you once we are
+            ready to launch our beta version. In the meantime, you can share it
+            and get a chance to earn 500 for early access to the platform.
+          </p>
+        </div>
         <button
           type="submit"
-
           className="f-PowerGrotesk bg-[#131515] text-base btn-color text-[#FFF5D9] leading-tight font-normal px-6 py-8 w-full h-[55px] mt-10 rounded-full flex items-center dark:bg-[#000000] justify-center"
-
         >
           Refer a friend
         </button>
       </div>
+
       <div className="mt-6 flex flex-col items-center text-[#6A9298] space-y-4 max-w-[272px]">
-        <h1 className="f-PowerGrotesk text-[15.5px] xxl:text-[17.5px] leading-[14.54px] text-[#6A929857]">Stay tuned</h1>
+        <h1 className="f-PowerGrotesk text-[15.5px] xxl:text-[17.5px] leading-[14.54px] text-[#6A929857]">
+          Stay tuned
+        </h1>
         <div>
           <ul className="flex space-x-3 justify-start items-center">
             <li>
@@ -46,11 +47,17 @@ const Refer = () => {
                 <img
                   src="/images/instagram.svg"
                   alt="Instagram"
-                  className="w-9 h-9" /></a>
+                  className="w-9 h-9"
+                />
+              </a>
             </li>
             <li>
               <a href="#" className="hover:text-white">
-                <img src="/images/linked.svg" alt="LinkedIn" className="w-9 h-9" />
+                <img
+                  src="/images/linked.svg"
+                  alt="LinkedIn"
+                  className="w-9 h-9"
+                />
               </a>
             </li>
             <li>
@@ -74,7 +81,11 @@ const Refer = () => {
             </li>
             <li>
               <a href="#" className="hover:text-white">
-                <img src="/images/hut.svg" alt="Your Icon" className="w-9 h-9" />
+                <img
+                  src="/images/hut.svg"
+                  alt="Your Icon"
+                  className="w-9 h-9"
+                />
               </a>
             </li>
           </ul>
