70401997df38fd2e5a46c067c9de19e6d08a3b12
shreyasCooasis <shreyas@cooasis.in>
2024-07-27 02:11:21 +0530

    Update nodemailer function to receive otp and content both as parameter

--- backend/utils/nodemailer.js
+++ backend/utils/nodemailer.js
@@ -17,17 +17,26 @@ const transporter = nodemailer.createTransport(smtpTransport({
     }
 }));
 
-const sendVerificationEmail = async (email, content) => {
+const sendVerificationEmail = async (email, otp = null, content = null) => {
+    let emailContent;
+
+    if (otp) {
+        emailContent = `Your OTP is: ${otp}`;
+    } else if (content) {
+        emailContent = content;
+    } else {
+        throw new Error('Either OTP or content must be provided');
+    }
+
     const mailOptions = {
         from: process.env.EMAIL_USER,
         to: email,
         subject: 'Verify your email',
-        text: content
+        text: emailContent
     };
 
     const info = await transporter.sendMail(mailOptions);
-
+    console.log('Email sent: ' + info.response);
 };
-
 
 module.exports = { sendVerificationEmail };
