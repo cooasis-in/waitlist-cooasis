1671118f29a6baba9638ae7861571ef21e0cb158
Shahbaz-cooasis <shahbaz@cooasis.in>
2024-07-30 11:13:40 +0530

    add favcon

--- frontend/src/components/ShareLinks.jsx
+++ frontend/src/components/ShareLinks.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import './pages.css'
+import "./pages.css";
 import {
   FacebookShareButton,
   WhatsappShareButton,
@@ -7,91 +7,107 @@ import {
   TwitterShareButton,
 } from "react-share";
 
-const ShareLinks = ({ setShowShareLink,sharelink }) => {
-  const shareUrl = sharelink; 
+const ShareLinks = ({ setShowShareLink, sharelink }) => {
+  const shareUrl = sharelink;
   const title = `Hope you’re doing awesome! 😊 I just signed up for cooasis, and they’re currently accepting early access waitlist. I thought you might want in too!
-Here’s a special referral link for you ${sharelink} Signing up through this link gives you a priority spot on the waitlist and helps me out too. Win-win!`
+Here’s a special referral link for you ${sharelink} Signing up through this link gives you a priority spot on the waitlist and helps me out too. Win-win!`;
 
   return (
     <div className="set-large-align w-[100%]">
-       <div className="card gradient-box mt-6 sm:mt-2 border-[1px] border-[#FFFFFF21] sm:mx-auto max-w-[100%] sm:max-w-[340px] md:max-w-[400px] lg:max-w-[340px] rounded-[40px] p-4 text-center shadow-lg relative z-10">
-      {/* 200 People Joined */}
-      <div className="mt-2 mb-4">
-        <div className="flex justify-center items-center">
-          <img
-            src="/images/avtar-1.svg"
-            alt="People Join"
-            className="overflow-hidden image-hover-effect"
-          />
-          <img
-            src="/images/avtar-2.svg"
-            alt="People Join"
-            className="ml-[-7px] overflow-hidden image-hover-effect"
-          />
-          <img
-            src="/images/avtar-3.svg"
-            alt="People Join"
-            className="ml-[-7px] overflow-hidden image-hover-effect"
-          />
+      <div className="card gradient-box mt-6 sm:mt-2 border-[1px] border-[#FFFFFF21] sm:mx-auto max-w-[100%] sm:max-w-[340px] md:max-w-[400px] lg:max-w-[340px] rounded-[40px] p-4 text-center shadow-lg relative z-10">
+        {/* 200 People Joined */}
+        <div className="mt-2 mb-4">
+          <div className="flex justify-center items-center">
+            <img
+              src="/images/avtar-1.svg"
+              alt="People Join"
+              className="overflow-hidden image-hover-effect"
+            />
+            <img
+              src="/images/avtar-2.svg"
+              alt="People Join"
+              className="ml-[-7px] overflow-hidden image-hover-effect"
+            />
+            <img
+              src="/images/avtar-3.svg"
+              alt="People Join"
+              className="ml-[-7px] overflow-hidden image-hover-effect"
+            />
+          </div>
+          <span className="f-PowerGrotesk text-[14.5px] text-[#FCFCD8] xxl:text-[17.5px] leading-[14.54px] flex justify-center items-center mt-1">
+            +200 people joined
+          </span>
         </div>
-        <span className="f-PowerGrotesk text-[14.5px] text-[#FCFCD8] xxl:text-[17.5px] leading-[14.54px] flex justify-center items-center mt-1">
-          +200 people joined
-        </span>
-      </div>
 
-     <div className="mt-6">
-       {/* Close Button button */}
-       <button
-        type="button"
-        className="absolute top-[18px] right-[18px] w-8 h-8 bg-black rounded-full flex items-center justify-center"
-        onClick={() => setShowShareLink(false)} 
-      >
-        <span className="text-white text-[25px] mb-1">×</span>
-      </button>
+        <div className="mt-6">
+          {/* Close Button button */}
+          <button
+            type="button"
+            className="absolute top-[18px] right-[18px] w-8 h-8 bg-black rounded-full flex items-center justify-center"
+            onClick={() => setShowShareLink(false)}
+          >
+            <span className="text-white text-[25px] mb-1">×</span>
+          </button>
 
-      {/* Instagram */}
-      <button
-        type="button"
-        onClick={() => {
-          navigator.clipboard.writeText(shareUrl);
-          alert("Link copied! Open Instagram and paste the link.");
-        }}
-        className="f-PowerGrotesk flex items-center bg-[#131515] mb-2 text-[12px] md:text-[12px] xxl:text-[17.5px] btn-color text-[#E1FF26] leading-tight font-normal px-6 py-4 w-full h-[55px] rounded-full dark:bg-[#000000] justify-center custom-button"
-      >
-        <img src="images/instagram-2.svg" alt="" className="mr-[8px] max-w-[25px]  xxl:max-w-[100%]" />
-        Share on Instagram
-      </button>
+          {/* Instagram */}
+          <button
+            type="button"
+            onClick={() => {
+              navigator.clipboard.writeText(shareUrl);
+              alert("Link copied! Open Instagram and paste the link.");
+            }}
+            className="f-PowerGrotesk flex items-center bg-[#131515] mb-2 text-[12px] md:text-[12px] xxl:text-[17.5px] btn-color text-[#E1FF26] leading-tight font-normal px-6 py-4 w-full h-[55px] rounded-full dark:bg-[#000000] justify-center custom-button"
+          >
+            <img
+              src="images/instagram-2.svg"
+              alt=""
+              className="mr-[8px] max-w-[25px]  xxl:max-w-[100%]"
+            />
+            Share on Instagram
+          </button>
 
-      {/* Facebook */}
-      <FacebookShareButton url={shareUrl} quote={title} className="w-full mb-2">
-        <button
-          type="button"
-          className="f-PowerGrotesk bg-[#131515] text-[12px] md:text-[12px] xxl:text-[17.5px] btn-color text-[#E1FF26] leading-tight font-normal px-6 py-4 w-full h-[55px] rounded-full flex items-center dark:bg-[#000000] justify-center custom-button"
-        >
-          <img src="images/face-book.svg" alt="" className="mr-[8px] max-w-[13px]  xxl:max-w-[100%]" />
-          Share on Facebook
-        </button>
-      </FacebookShareButton>
+          {/* Facebook */}
+          <FacebookShareButton
+            url={shareUrl}
+            quote={title}
+            className="w-full mb-2"
+          >
+            <button
+              type="button"
+              className="f-PowerGrotesk bg-[#131515] text-[12px] md:text-[12px] xxl:text-[17.5px] btn-color text-[#E1FF26] leading-tight font-normal px-6 py-4 w-full h-[55px] rounded-full flex items-center dark:bg-[#000000] justify-center custom-button"
+            >
+              <img
+                src="images/face-book.svg"
+                alt=""
+                className="mr-[8px] max-w-[13px]  xxl:max-w-[100%]"
+              />
+              Share on Facebook
+            </button>
+          </FacebookShareButton>
 
-      {/* Whatsapp */}
-      <WhatsappShareButton
-        url={shareUrl}
-        title={title}
-        separator=":: "
-        className="w-full mb-2"
-      >
-        <button
-          type="button"
-          className="f-PowerGrotesk bg-[#131515] text-[12px] md:text-[12px] xxl:text-[17.5px] btn-color text-[#E1FF26] leading-tight font-normal px-6 py-4 w-full h-[55px] rounded-full flex items-center dark:bg-[#000000] justify-center custom-button"
-        >
-          <img src="images/whats-app.svg" alt="" className="mr-[8px] max-w-[13px]  xxl:max-w-[100%]" />
-          Share on Whatsapp
-        </button>
-      </WhatsappShareButton>
-     </div>
+          {/* Whatsapp */}
+          <WhatsappShareButton
+            url={shareUrl}
+            title={title}
+            separator=":: "
+            className="w-full mb-2"
+          >
+            <button
+              type="button"
+              className="f-PowerGrotesk bg-[#131515] text-[12px] md:text-[12px] xxl:text-[17.5px] btn-color text-[#E1FF26] leading-tight font-normal px-6 py-4 w-full h-[55px] rounded-full flex items-center dark:bg-[#000000] justify-center custom-button"
+            >
+              <img
+                src="images/whats-app.svg"
+                alt=""
+                className="mr-[8px] max-w-[13px]  xxl:max-w-[100%]"
+              />
+              Share on Whatsapp
+            </button>
+          </WhatsappShareButton>
+        </div>
 
-      {/* Email */}
-      {/* <EmailShareButton
+        {/* Email */}
+        {/* <EmailShareButton
           url={shareUrl}
           subject={title}
           body="Check this out:"
@@ -105,19 +121,22 @@ Here’s a special referral link for you ${sharelink} Signing up through this li
           </button>
         </EmailShareButton> */}
 
-      {/* Twitter */}
-      <TwitterShareButton url={shareUrl} title={title} className="w-full">
-        <button
-          type="button"
-          className="f-PowerGrotesk bg-[#131515] text-[12px] md:text-[12px] xxl:text-[17.5px] btn-color text-[#E1FF26] leading-tight font-normal py-2 px-6 sm:py-4 w-full h-[55px] rounded-full flex items-center dark:bg-[#000000] justify-center custom-button"
-        >
-          <img src="images/insta-gram.svg" alt="" className="mr-[8px] max-w-[13px]  xxl:max-w-[100%]" />
-          Share on email
-        </button>
-      </TwitterShareButton>
+        {/* Twitter */}
+        <TwitterShareButton url={shareUrl} title={title} className="w-full">
+          <button
+            type="button"
+            className="f-PowerGrotesk bg-[#131515] text-[12px] md:text-[12px] xxl:text-[17.5px] btn-color text-[#E1FF26] leading-tight font-normal py-2 px-6 sm:py-4 w-full h-[55px] rounded-full flex items-center dark:bg-[#000000] justify-center custom-button"
+          >
+            <img
+              src="images/insta-gram.svg"
+              alt=""
+              className="mr-[8px] max-w-[13px]  xxl:max-w-[100%]"
+            />
+            Share on email
+          </button>
+        </TwitterShareButton>
+      </div>
     </div>
-    </div>
-   
   );
 };
 
