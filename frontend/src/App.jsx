fd52281f628be4c277d6269a560386c260365612
TaslimCooasis <taslim@cooasis.in>
2024-08-07 19:09:02 +0530

    add mobile pages

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -16,8 +16,8 @@ function App() {
     <div className="">
       <Routes>
         <Route path="/" element={<NumberPage />} />
-        <Route path="/mobileverify" element={<NumberVerify />} />
-        <Route path="/landingpage" element={<EmailPage />} />
+        <Route path="/numberverify" element={<NumberVerify />} />
+        <Route path="/emailgpage" element={<EmailPage />} />
         <Route path="/share" element={<ShareLinks />} />
         <Route path="/refer" element={<Refer />} />
         <Route path="/waitlist" element={<Waitlist />} />
