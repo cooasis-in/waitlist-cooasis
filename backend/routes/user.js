dba77b7885074e35ab222eeb85bd804860b22504
shreyasCooasis <shreyas@cooasis.in>
2024-08-10 16:31:14 +0530

    added route fro phone number

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -1,10 +1,11 @@
 
 const express = require('express');
 const router = express.Router();
-const { createUser, resendOtp, verifyOTP } = require('../controllers/user');
+const { createUser, resendOtp, verifyOTP, updateUserPhoneNumber } = require('../controllers/user');
 
 router.post('/users', createUser);
 router.post('/resend-otp', resendOtp);
 router.post('/verify-email', verifyOTP);
+router.post('/updatePhoneNumber', updateUserPhoneNumber);
 
 module.exports = router;
